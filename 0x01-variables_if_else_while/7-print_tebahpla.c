9710812b538289e467164caea22d074e201ac352
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-07-14 13:48:27 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - to print random with printf
+ *
+ * Return: Always 0.
+**/
+
+int main(void)
+{
+	int lwr;
+
+	lwr = 'z';
+
+	while (lwr <= 'a')
+	{
+		putchar(lwr);
+		lwr--;
+	}
+		putchar ('\n');
+	return (0);
+}
