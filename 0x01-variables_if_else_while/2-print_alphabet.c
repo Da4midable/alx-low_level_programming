bff72916692c2c7d69edca542e2f716d1f7d5a9c
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-07-14 07:08:45 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - to print random with printf
+ *
+ * Return: Always 0.
+**/
+
+int main(void)
+{
+	int lwr;
+
+	for (lwr = 'a'; lwr <= z; lwr++)
+		putchar(lwr);
+	return (0);
+}
