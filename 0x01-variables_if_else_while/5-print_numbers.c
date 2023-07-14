b893aace2771d98fba977b3d76802dd606907c0d
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-07-14 12:54:45 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - to print random with printf
+ *
+ * Return: Always 0.
+**/
+
+int main(void)
+{
+	int dg;
+
+	for (dg = 0; dg <= 10; dg++)
+	{
+		printf("%d", dg);
+	}
+		printf("\n");
+
+	return (0);
+}
