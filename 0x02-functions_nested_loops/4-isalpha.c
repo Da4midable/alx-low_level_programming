57311b1c2dda0b5c80346349ca4c8d737bba88b2
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-07-18 12:53:32 -0700

    Task 4 checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - displays true or false value for lowercase letters
+ * @c: the character to be checked
+ * Return: int c
+ */
+
+int _isalpha(int c)
+{
+
+	if (c > 65 && c < 123)
+{
+	return (1);
+}
+
+	return (0);
+
+}
