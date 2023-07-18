a3a39d33e8c8cd7372a2f6a0db7d2b01759a0880
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-07-18 15:36:17 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - computes the absolute value of an integer
+ * @n: computes the absolute value
+ * Return: Absolute value to be computed
+ */
+
+int print_last_digit(int n)
+{
+	if (n < 0 || n == 0 || n > 0)
+{
+	int lad;
+
+	lad = n % 10;
+
+	return (lad);
+}
+
+	return (n);
+}
