5baee1fe707cec242caa1ebdc0f8ae54732bcbb9
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-07-18 09:25:13 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - displays lowercase letters
+ * Return: void
+ */
+
+
+void print_alphabet(void)	/*print_alphabet displays lowercase letters*/
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+{
+
+	_putchar(c);
+
+}
+	_putchar('\n');
+
+}
