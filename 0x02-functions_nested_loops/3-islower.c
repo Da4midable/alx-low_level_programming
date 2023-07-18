6db0e3e7c10830681a61519ce6de2b7fd83d494b
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-07-18 12:34:28 -0700

    Task 3 is  a function that checks for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - displays true or false value for lowercase letters
+ * @c: the character to be checked
+ * Return: int c
+ */
+
+int _islower(int c)
+{
+
+	if (c > 96 && c < 123)
+{
+	return (1);
+}
+
+	return (0);
+
+}
