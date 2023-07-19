16901bec4eef2a06ec7b7ea085999f469149a5fa
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-07-19 07:47:59 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+
+/**
+ * times_table - displays lowercase letters
+ * Return: void
+ */
+
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 10; i++)
+{
+	for (j = 0; j <= 9; j++)
+{
+	k = i * j;
+
+		_putchar(k);
+	if (k / 10 > 0)
+{
+		_putchar((k / 10) + '0');
+}
+	else if (j > 0)
+{
+	_putchar(' ');
+}
+	_putchar((k % 10) + '0');
+
+	if (j < 9)
+{
+	_putchar(',');
+	_putchar(' ');
+}
+	else
+{
+	_putchar('$');
+}
+}
+		_putchar('\n');
+}
+}
