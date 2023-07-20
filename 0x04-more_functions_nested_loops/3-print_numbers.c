f52cfe2487195b0b7f974a3030a3d66a106fe0eb
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-07-20 03:00:29 -0700

    Task 3 prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_numbers - computes the sum of two integers
+* Return: Always void
+*/
+
+void print_numbers(void)
+{
+int d;
+
+d = 0;
+
+while (d < 10)
+{	_putchar(d + '0');
+	d++;
+}
+	_putchar('\n');
+
+}
