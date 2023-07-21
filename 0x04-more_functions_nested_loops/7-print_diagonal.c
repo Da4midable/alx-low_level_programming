48a04e32c5f20c0c2a9ce618271cfa037ab77feb
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-07-21 05:05:37 -0700

    Task 7  draws a diagonal line on the terminal.

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* print_diagonal - computes the sum of two integers
+* @n: does whatever
+* Return: Always void
+*/
+
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+	
+
+	if (n <= 0)
+{
+	_putchar('\n');
+}
+	else
+{
+	for (a = 0; a < n; a++)
+{
+	for (b = 0; b < a; b++)
+{
+	_putchar(32);
+}
+	_putchar(92);
+	_putchar('\n');
+}
+}
+}
