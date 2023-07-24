d75cb754f69e7606e4d8e4db49e1c68f851f30a8
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-07-24 09:30:34 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* reset_to_98 - resets everything to 98
+* @n: This declare the pointer to an integer
+* Return: Always 0.
+*/
+
+	void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
