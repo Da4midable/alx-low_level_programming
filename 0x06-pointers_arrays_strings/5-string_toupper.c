39341297c8f5b4afaadc8d79052b927e5b2e27fc
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-07-27 16:13:50 -0700

    Task 5 Updates to Uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,12 +10,14 @@ char *string_toupper(char *up)
 {
 	int index;
 
+	index = 0;
+
 	while (up[index] != '\0')
 	{
 		if (up[index] >= 'a' && up[index] <= 'z')
-	{
+
 		up[index] = (up[index] - 32);
-	}
+
 		index++;
 
 	}
