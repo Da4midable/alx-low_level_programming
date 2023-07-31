f7d119661911efeaddaaa3d182ee14b19468b492
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-07-31 13:26:41 -0700

    Task 2 Searches for First Instance of Char C

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* *_strchr - searches a specific character
+* @s: pointer
+* @c: character to be searched
+* Return: Always null.
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i])
+{
+	if (s[i] == c)
+{
+	return (&s[i]);
+}
+	i++;
+}
+	return ('\0');
+}
