9a8da94ba4503e1ded1c92a43465c8ef303b8159
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-08-08 08:23:33 -0700

    Task 0: Malloc and Ptr Init to C

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
 
-	void *zeroArray = malloc(sizeof(char) * (size + 1));
+	void *zeroArray = malloc(sizeof(char) * (size));
 	char *z = (char *)zeroArray;
 
 
