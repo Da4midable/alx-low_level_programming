c20130613765e20a659d5573eed8eb07164e5f67
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-08-08 16:14:33 -0700

    Task 2 Dynamically Concatenates

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* **str_concat - duplicates string
+* @s1: first string
+* @s2: second string
+* Return: .
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0, len = 0;
+	char *c;
+
+	while (s1[i] != '\0')
+	{
+		i++;
+		len += i;
+	}
+	while (s2[i] != '\0')
+	{
+		i++;
+		len += i;
+	}
+	c = malloc((sizeof(char) * len) + 1);
+	if (c == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (s1[j] != '\0')
+	{
+		c[i] = s1[j];
+		i++;
+		j++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		c[i] = s2[j];
+		i++;
+		j++;
+	}
+		c[i] = '\0';
+	return (c);
+	free(c);
+}
