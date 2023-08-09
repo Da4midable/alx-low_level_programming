b8112d26b08605316fda8a2e5f6b69be0ea9d8b8
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-08-09 10:02:50 -0700

    Task 3: Two-dimensional Array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* **alloc_grid - duplicates string
+* @width: row array
+* @height: column array
+* Return: ar.
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, a, b;
+	int **ar;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	for (i = 0; i < width; i++)
+	{
+		ar = malloc(width * sizeof(int *));
+	}
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < width; i++)
+	{
+		ar[i] = malloc(height * sizeof(int));
+
+		if (ar[i] == NULL)
+		{
+
+		for (j = i; j >= 0; j--)
+		{
+			free(ar[j]);
+		}
+		free(ar);
+		return (NULL);
+		}
+	}
+	for (a = 0; a < width; a++)
+	{
+		for (b = 0; b < height; b++)
+		{
+			ar[a][b] = 0;
+		}
+	}
+	return (ar);
+}
