68cfa63fdf5f5d9c94bf6fa4935b6785dd50a825
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-08-12 03:59:32 -0700

    Task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* *_calloc - to imitate stdlib calloc
+* @nmemb: number of array members
+* @size: size of each element of array
+* Return: pointer.
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *cast;
+
+	if (nmemb <= 0 || size <= 0)
+	{
+		return (NULL);
+	}
+		cast = malloc(nmemb * size);
+
+	if (cast == NULL)
+	{
+		return (NULL);
+	}
+	return (cast);
+}
