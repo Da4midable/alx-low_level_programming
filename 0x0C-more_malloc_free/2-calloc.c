52e6705d97eb73e20dc3105cf0f0b662ee445e9d
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-08-12 04:21:58 -0700

    Task 2 Mimics Calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,18 +10,26 @@
 */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	void *cast;
+{	int i = 0, j = 0;
+	char *cast;
+
+	j = nmemb * size;
 
 	if (nmemb <= 0 || size <= 0)
 	{
 		return (NULL);
 	}
-		cast = malloc(nmemb * size);
+		cast = malloc(j);
 
 	if (cast == NULL)
 	{
 		return (NULL);
+	}
+
+	while (i < j)
+	{
+		cast[i] = 0;
+		i++;
 	}
 	return (cast);
 }
