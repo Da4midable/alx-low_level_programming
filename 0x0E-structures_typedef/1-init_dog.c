0878ee88f6f488b449bfcd8eeb1ec148837a64a2
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-08-14 11:40:57 -0700

    Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+#include "main.h"
+
+/**
+ * init_dog - function to call struct dog
+ * @d: pointer
+ * @name: name argument
+ * @age: age argument
+ * @owner: owner argument
+ */
+
+struct dog;
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
