ead6a871b41d4dc5b896b60aa89165679238c985
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-08-14 10:03:56 -0700

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,17 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - this a struct for an animal
+ * @name: name argument
+ * @age: age argument
+ * @owner: owner argument
+ */
 
-	struct dog	
-	{
-		char *name;
-		float age;
-		char *owner;
-	};
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
