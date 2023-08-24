3c4145a82d85b3722247ec18f761d3cb1a95c782
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-08-24 04:48:35 -0700

    Task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - function to call struct dog
+ * @d: pointer
+ * @name: name argument
+ * @age: age argument
+ * @owner: owner argument
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	name = new_dog.name;
+	owner = new_dog.owner;
+
+	if (new_dog == NULL)
+		return(NULL);
+}
