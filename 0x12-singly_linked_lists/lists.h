337b67b3c5ee906f4898204b653f02189b525baf
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-08-25 18:43:55 +0100

    Task 0 Fixed

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,19 +8,20 @@
 
 
 /**
- * struct dog - this a struct for an animal
- * @name: name argument
- * @age: age argument
- * @owner: owner argument
- */
+ ** struct list_s - singly linked list
+ ** @str: string - (malloc'ed string)
+ ** @len: length of the string
+ ** @next: points to the next node
+ **
+ ** Description: singly linked list node structure
+ **/
 
-	struct node {
-		char *str;
-		size_t len;
-		struct node* next;
-};
-
-typedef struct node list_t;
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 
