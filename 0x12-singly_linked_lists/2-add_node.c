ea9e3442c4f8a61a4be85a1c2bb097a63cacc5b5
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-08-28 12:19:01 +0100

    Task 2 Adds Node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,24 +1,40 @@
 #include "lists.h"
 
 /**
- * list_len - prints the elements of a linked list
- * @h: is the pointer to the head node
- * Return: count.
+ * add_node - add nodes at the beginning of a linked list
+ * @head: This is the double pointer for changing value of first node
+ * @str: This is the string to be duplicated
+ * Return: current.
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t **current;
+	list_t *beg_node;
 
-	str = strdup(&head->str);
-	head->next = current;
-	current = head;
-
-	while (current != NULL)
+	if (str == NULL)
 	{
-
-		current = current->next;
-
+		return (NULL);
 	}
-		return (head);
+
+	beg_node = (struct list_s *)malloc(sizeof(list_t));
+
+	if (beg_node == NULL)
+	{
+		return (NULL);
+	}
+
+
+	beg_node->str = strdup(str);
+
+	if (beg_node->str == NULL)
+	{
+		free(beg_node);
+		return (NULL);
+	}
+		beg_node->len = strlen(str);
+		beg_node->next = *head;
+		*head = beg_node;
+
+
+		return (beg_node);
 }
