a4a1a140d8f67843c93a755b3460b279ec5bf876
Saheed Kehinde Yusuf (SKY) <saheed.k.yusuf@gmail.com>
2023-08-28 21:12:33 +0100

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list and its nodes
+ * @head: Pointer to the head node of the list
+ */
+
+void free_listint2(listint_t **head)
+{
+
+	listint_t *current, *buffer;
+
+	current = *head;
+
+	while (current != NULL)
+	{
+		buffer = current->next;
+		free(current);
+		current = buffer;
+	}
+	*head = NULL;
+
+}
