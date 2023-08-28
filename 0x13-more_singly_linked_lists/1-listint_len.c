c75aa5d94b308da18d1402d0a16d010d3a94bd4c
Saheed Kehinde Yusuf (SKY) <saheed.k.yusuf@gmail.com>
2023-08-28 18:50:22 +0100

    Task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * listint_len - prints the elements of a linked list
+ * @h: is the pointer to the head node
+ * Return: count.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+	const listint_t *current = h;
+
+	while (current != NULL)
+	{
+
+		current = current->next;
+		count++;
+
+	}
+
+	return (count);
+}
