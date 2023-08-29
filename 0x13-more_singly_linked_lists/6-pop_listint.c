611238a524c9a0c096e35f8be65b373629b6668d
Saheed Kehinde Yusuf (SKY) <saheed.k.yusuf@gmail.com>
2023-08-29 11:49:47 +0100

    Task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes nodes at the beginning of a linked list
+ * @head: This is the double pointer for traversing the list from first node
+ * Return: n.
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *current, *buffer;
+
+		current = *head;
+
+	if (current == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		buffer = current;
+		current = current->next;
+		buffer = NULL;
+		free(buffer);
+	}
+		return (current->n);
+}
