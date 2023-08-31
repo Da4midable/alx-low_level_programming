bb55953a4161146a2f208860a2f7ddd2aff0c227
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-08-31 17:28:21 +0100

    Task 0 Converts Binary to Integer

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_
+#define _MAIN_
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -8,7 +8,7 @@
 
 #define BUFF_SIZE 1024
 
-
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
