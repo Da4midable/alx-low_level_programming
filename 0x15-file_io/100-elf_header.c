62fbe0cda37ad9cdec9d8e40623e7f9576961d3a
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2023-10-02 17:57:18 +0100

    ELF Update 2

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,51 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <fcntl.h>
-#include <elf.h>
+#include <unistd.h>
+#include <stdint.h>
 
-void error(char *progname, char *filename) {
-    fprintf(stderr, "%s: Error: %s is not an ELF file\n", progname, filename);
-    exit(98);
-}
+#define EI_NIDENT 16
 
-int main(int argc, char **argv) {
+typedef struct {
+    unsigned char e_ident[EI_NIDENT];
+    uint16_t e_type;
+    uint16_t e_machine;
+    uint32_t e_version;
+    uint64_t e_entry;
+    uint64_t e_phoff;
+    uint64_t e_shoff;
+    uint32_t e_flags;
+    uint16_t e_ehsize;
+    uint16_t e_phentsize;
+    uint16_t e_phnum;
+    uint16_t e_shentsize;
+    uint16_t e_shnum;
+    uint16_t e_shstrndx;
+} Elf64_Ehdr;
+
+void print_elf_header_info(const Elf64_Ehdr *elf_header);
+
+int main(int argc, char *argv[]) {
     int fd;
-    ssize_t bytes;
-    char magic[4];
-    Elf64_Ehdr header;
+	ssize_t bytes_read;
+    Elf64_Ehdr elf_header;
 
     if (argc != 2) {
         fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
-        return (1);
+        return 98;
     }
 
     fd = open(argv[1], O_RDONLY);
     if (fd == -1) {
-        perror("Error opening file");
-        return (98);
+        perror("Error");
+        return 98;
     }
 
-    bytes = read(fd, magic, 4);
-    if (bytes != 4 || magic[0] != 0x7f || magic[1] != 'E' || magic[2] != 'L' || magic[3] != 'F') {
-        error(argv[0], argv[1]);
+    bytes_read = read(fd, &elf_header, sizeof(Elf64_Ehdr));
+    if (bytes_read != sizeof(Elf64_Ehdr) || elf_header.e_ident[0] != 0x7F || elf_header.e_ident[1] != 'E' || elf_header.e_ident[2] != 'L' || elf_header.e_ident[3] != 'F') {
+        fprintf(stderr, "Error: Not an ELF file\n");
+        close(fd);
+        return 98;
     }
 
-    lseek(fd, 0, SEEK_SET);
-    bytes = read(fd, &header, sizeof(header));
-    if (bytes != sizeof(header)) {
-        error(argv[0], argv[1]);
-    }
-
-    printf("Magic:   %.2x %.2x %.2x %.2x\n", magic[0], magic[1], magic[2], magic[3]);
-    printf("Class:   %s\n", header.e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
-    printf("Data:    %s\n", header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-    printf("Version: %d %s\n", header.e_ident[EI_VERSION], "(current)");
-    printf("OS/ABI:  UNIX - System V\n");
-    printf("ABI Version: %d\n", header.e_ident[EI_OSABI]);
-    printf("Type:    %s\n", header.e_type == ET_EXEC ? "EXEC (Executable file)" : "Unknown");
-    printf("Entry point address:               0x%lx\n", header.e_entry);
+    print_elf_header_info(&elf_header);
 
     close(fd);
-    return (0);
+    return 0;
+}
+
+void print_elf_header_info(const Elf64_Ehdr *elf_header) {
+	int i;
+
+	printf("Magic:   ");
+    for (i = 0; i < EI_NIDENT; i++) {
+        printf("%02x ", elf_header->e_ident[i]);
+    }
+    printf("\nClass:   %s\n", elf_header->e_ident[4] == 1 ? "ELF32" : "ELF64");
+    printf("Data:    %s\n", elf_header->e_ident[5] == 1 ? "2's complement, little endian" : "2's complement, big endian");
+    printf("Version: %d (current)\n", elf_header->e_ident[6]);
+    printf("OS/ABI:  %d\n", elf_header->e_ident[7]);
+    printf("ABI Version: %d\n", elf_header->e_ident[8]);
+    printf("Type:    %d\n", elf_header->e_type);
+    printf("Entry point address: %lx\n", elf_header->e_entry);
 }
