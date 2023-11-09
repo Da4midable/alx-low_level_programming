07be9f98c7abe710f7380f6c9c2e0b934c3bcddb
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2023-11-09 19:01:20 +0100

    Function Frees a dlistint_t List

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 
 /**
- * free_list - frees a linked list and its nodes
+ * free_dlistint - frees a linked list and its nodes
  * @head: Pointer to the head node of the list
  */
 
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *current;
-	
+
 	if (head == NULL)
 	{
 		return;
