ff03b50072eff570141bbde5e6af2ff3abbcdaf2
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2023-11-09 15:58:39 +0100

    Adds Node at the Tip of a Doubly Linked List

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add a node at the beginning of a doubly linked list
+ * @head: Double pointer for inserting new node at the tip of node
+ * @n: integer element of node
+ * Return: current.
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *beg_node;
+
+	beg_node = malloc(sizeof(dlistint_t));
+
+	if (beg_node == NULL)
+	{
+		return (NULL);
+	}
+
+	beg_node->n = n;
+	beg_node->next = *head;
+	*head = beg_node;
+
+	return (beg_node);
+}
