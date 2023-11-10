e66d7bd754681ac1b9641bfc2db9c2f4a74e1321
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2023-11-10 20:45:43 +0100

    Inserts Node at a Given Index in a Linked List

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,39 +10,23 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *current;
+	dlistint_t *new, *current = *h;
 	unsigned int count = 0;
 
-	if (h == NULL)
-	{
-		return (NULL);
-	}
+	if (idx == 0)
+	return (add_dnodeint(h, n));
 
 	new = malloc(sizeof(dlistint_t));
-
 	if (new == NULL)
 	{
 		return (NULL);
 	}
-
 	new->n = n;
-
-	if (idx == 0)
-	{
-		new->next = *h;
-		*h = new;
-		return (new);
-	}
 	current = *h;
 	while (current != NULL && count < idx - 1)
 	{
 		current = current->next;
 		count++;
-	}
-	if (current == NULL)
-	{
-		free(new);
-		return (NULL);
 	}
 	new->next = current->next;
 	new->prev = current;
