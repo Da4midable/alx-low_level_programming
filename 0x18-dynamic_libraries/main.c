9fd8327e00ffa4889b7e9197521c72e0e165c5d5
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-11-13 19:15:01 +0100

    Dynamic Library libdynamic.so Containing Select Functions

--- 0x18-dynamic_libraries/main.c
+++ 0x18-dynamic_libraries/main.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
-
 /**
  * main - check the code
  *
  * Return: Always EXIT_SUCCESS.
  */
+
 int main(void)
 {
-    printf("%d\n", _strlen("My Dyn Lib"));
-    return (EXIT_SUCCESS);
+	printf("%d\n", _strlen("My Dyn Lib"));
+	return (EXIT_SUCCESS);
 }
