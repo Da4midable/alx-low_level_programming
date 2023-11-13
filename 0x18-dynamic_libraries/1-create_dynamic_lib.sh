91d3d159842c416acb83252133b4c91a887fd47e
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2023-11-13 11:36:58 -0800

    A Script that Creates a Dynamic Library from All .c Files - 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-gcc -c *.c -fpic && gcc *.o -shared -o liball.so
+gcc -Wall -pedantic -Werror -Wextra -c -fpic *.c
+gcc *.o -shared -o liball.so
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
