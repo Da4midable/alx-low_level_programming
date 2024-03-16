eb6a140f5cf77ac59a0bfe8816c91d3234962e1f
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2024-03-16 12:24:49 +0100

    Advanced Binary Search algorithm

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -13,10 +13,10 @@ int advanced_binary_helper(int *array, size_t start, size_t end, int value);
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-    if (array == NULL || size == 0)
-        return (-1);
+	if (array == NULL || size == 0)
+		return (-1);
 
-    return advanced_binary_helper(array, 0, size - 1, value);
+	return (advanced_binary_helper(array, 0, size - 1, value));
 }
 
 /**
@@ -30,31 +30,31 @@ int advanced_binary(int *array, size_t size, int value)
 
 int advanced_binary_helper(int *array, size_t start, size_t end, int value)
 {
-    size_t mid;
+	size_t mid;
 	size_t i;
 
-    if (start > end)
-        return (-1);
+	if (start > end)
+		return (-1);
 
-    printf("Searching in array: ");
-    for (i = start; i <= end; i++)
-    {
-        printf("%d", array[i]);
-        if (i < end)
-            printf(", ");
-    }
-    printf("\n");
+	printf("Searching in array: ");
+	for (i = start; i <= end; i++)
+	{
+		printf("%d", array[i]);
+		if (i < end)
+			printf(", ");
+	}
+	printf("\n");
 
-    mid = start + (end - start) / 2;
-    if (array[mid] == value)
-    {
-        if (mid == start || array[mid - 1] != value)
-            return (int)mid;
-        else
-            return advanced_binary_helper(array, start, mid, value);
-    }
-    else if (array[mid] < value)
-        return advanced_binary_helper(array, mid + 1, end, value);
-    else
-        return advanced_binary_helper(array, start, mid - 1, value);
+	mid = start + (end - start) / 2;
+	if (array[mid] == value)
+	{
+		if (mid == start || array[mid - 1] != value)
+			return ((int)mid);
+		else
+			return (advanced_binary_helper(array, start, mid, value));
+	}
+	else if (array[mid] < value)
+		return (advanced_binary_helper(array, mid + 1, end, value));
+	else
+		return (advanced_binary_helper(array, start, mid - 1, value));
 }
